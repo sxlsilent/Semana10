a18b44968e67a54fc2ad13841a32ca236b385266
sxlsilent <maldonadobrandon236@gmail.com>
2026-03-24 21:19:54 -0500

    ej8Examen.cpp

---
+++ ej8Examen.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b;
+
+    cout << "Ingrese dos numeros: ";
+    cin >> a >> b;
+
+    cout << "Suma: " << (a + b) << endl;
+    cout << "Resta: " << (a - b) << endl;
+    cout << "Multiplicacion: " << (a * b) << endl;
+
+    return 0;
+}
+
+
+/* El programa corrio tal y como estaba en el examen.*/
